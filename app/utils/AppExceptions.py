3fc48f7fcb5a744463dec7b1d70c41e80830bb11
Sughan Richardson Samson Babuji <37295583+sughan1996@users.noreply.github.com>
2026-05-17 09:51:18 -0400

    fixed

--- app/utils/AppExceptions.py
+++
@@ -1,8 +0,0 @@
-class AppBaseException(Exception):
-    """Base exception for the application."""
-    def __init__(self, message: str):
-        super().__init__(message)
-        self.message = message
-
-    def __call__(self, *args, **kwargs):
-        return self.message
